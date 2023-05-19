a364a112b713560be1daaa1dde68913a8006a4f5
tiAxor <ti@axor.com.br>
2023-05-19 09:05:04 -0300

    feat: accessing firefox

--- index.py
+++ index.py
@@ -12,10 +12,13 @@ password = os.getenv("PASSWORD")
 
 
 def access_site_by_chrome(site):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless=new")
-    # chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=chrome_options)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless=new")
+    # # chrome_options.add_experimental_option("detach", True)
+    # driver = webdriver.Chrome(options=chrome_options)
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
     driver.get(site)
     # input de keys
     driver.find_element("id", "btnMarcarPonto").click()
@@ -35,12 +38,12 @@ if __name__ == "__main__":
         today = datetime.today().strftime("%d-%m-%Y %H:%M:%S")
         # acessar site
         access_site_by_chrome("https://www.mdcomune.com.br/")
-        with open(".\log.txt", "a") as log:
+        with open("log.txt", "a") as log:
             log.write(f"{today}: ok\n")
             print(f"{today}: ok")
             log.close()
     except WebDriverException as e:
-        with open(".\log.txt", "a") as log:
+        with open("log.txt", "a") as log:
             log.write(f"{today}: {e.msg}")
             print(f"{today}: {e.msg}")
             log.close()
